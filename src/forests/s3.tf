4a743e4c756a6baa9b699574cfcec634271e0106
Livnoni <yehuda.livnoni@gmail.com>
2021-12-01 17:33:16 +0200

    Update src/forests/s3.tf
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- src/forests/s3.tf
+++ src/forests/s3.tf
@@ -81,4 +81,11 @@ resource "aws_s3_bucket" "logs" {
     
     Environment = local.resource_prefix.value
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
