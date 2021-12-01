dd09dc4cd1f73cdbf382dcf6e780124f557fdc6c
Livnoni <yehuda.livnoni@gmail.com>
2021-12-01 12:38:33 +0200

    Update src/new.tf
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- src/new.tf
+++ src/new.tf
@@ -1,2 +1,9 @@
 resource "aws_s3_bucket" "example1-noblocks" {
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "fake"
+      }
+    }
+  }
 }
